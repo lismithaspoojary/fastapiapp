a4b84ddc791775fce9d1b8fd1b916a69142ef6a6
lismitha <lismithaspoojary@gmail.com>
2026-06-29 17:47:43 +1200

    updated project files

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI
 
 from routers import company,job
 from database import Base,engine
+from models import job as job_model,company as company_model
 
 app = FastAPI()
 print(engine)
 
-Base.metadata.create_all(bind=engine)
+#Base.metadata.create_all(bind=engine)
 
 app.include_router(company.router)
 app.include_router(job.router)
